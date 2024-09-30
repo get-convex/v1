489c2beec13070da56f7babb3570f8fea05c726e
Shawn Erquhart <shawn@erquh.art>
2024-09-30 14:54:55 -0400

    port navigation and controls from convex-saas

--- apps/app/src/app/[locale]/(dashboard)/layout.tsx
+++ apps/app/src/app/[locale]/(dashboard)/layout.tsx
@@ -1,4 +1,23 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
-  // This is where your authenticated app lives, add a sidebar, header etc.
-  return children;
+import { convexAuthNextjsToken } from "@convex-dev/auth/nextjs/server";
+import { api } from "@v1/backend/convex/_generated/api";
+import { fetchQuery } from "convex/nextjs";
+import { Navigation } from "./_components/navigation";
+
+export default async function Layout({
+  children,
+}: { children: React.ReactNode }) {
+  const user = await fetchQuery(
+    api.users.getUser,
+    {},
+    { token: convexAuthNextjsToken() },
+  );
+  if (!user) {
+    return null;
+  }
+  return (
+    <div className="flex min-h-[100vh] w-full flex-col bg-secondary dark:bg-black">
+      <Navigation user={user} />
+      {children}
+    </div>
+  );
 }
