515e871bfd83c9b3fb0d36a8b884063cf750842e
Shawn Erquhart <shawn@erquh.art>
2024-10-03 15:18:12 -0400

    simplify loading screen

--- apps/app/src/app/[locale]/onboarding/layout.tsx
+++ apps/app/src/app/[locale]/onboarding/layout.tsx
@@ -1,7 +1,7 @@
-import { Spinner } from "@/components/Spinner";
 import { convexAuthNextjsToken } from "@convex-dev/auth/nextjs/server";
 import { api } from "@v1/backend/convex/_generated/api";
 import { fetchAction, fetchMutation, fetchQuery } from "convex/nextjs";
+import { Loader2 } from "lucide-react";
 import Image from "next/image";
 import { redirect } from "next/navigation";
 
@@ -34,17 +34,23 @@ export default async function Layout({
     return null;
   }
 
+  const subscriptionPending = user.username && user.polarSubscriptionPendingId;
+
   return (
     <div className="relative flex h-screen w-full bg-card">
       <div className="absolute left-1/2 top-8 mx-auto -translate-x-1/2 transform justify-center">
         <Image src="/logo.png" alt="logo" width={100} height={100} />
       </div>
       <div className="z-10 h-screen w-screen">
-        {user.username && user.polarSubscriptionPendingId ? (
-          <Spinner />
-        ) : (
-          children
+        {subscriptionPending && (
+          <div className="flex h-screen w-screen flex-col items-center justify-center gap-4">
+            <Loader2 className="h-8 w-8 animate-spin text-primary" />
+            <p className="text-center text-base font-normal text-primary/60">
+              Processing your subscription. This may take a moment...
+            </p>
+          </div>
         )}
+        {!subscriptionPending && children}
       </div>
       <div className="base-grid fixed h-screen w-screen opacity-40" />
       <div className="fixed bottom-0 h-screen w-screen bg-gradient-to-t from-[hsl(var(--card))] to-transparent" />
