a83fc13ebe454a78e789f2b200e24e36861fa585
Shawn Erquhart <shawn@erquh.art>
2024-10-03 14:24:01 -0400

    add subscription pending spinner

--- apps/app/src/app/[locale]/onboarding/layout.tsx
+++ apps/app/src/app/[locale]/onboarding/layout.tsx
@@ -1,3 +1,4 @@
+import { Spinner } from "@/components/Spinner";
 import { convexAuthNextjsToken } from "@convex-dev/auth/nextjs/server";
 import { api } from "@v1/backend/convex/_generated/api";
 import { fetchAction, fetchMutation, fetchQuery } from "convex/nextjs";
@@ -28,12 +29,15 @@ export default async function Layout({
     );
     return redirect(checkoutUrl);
   }
+
   return (
     <div className="relative flex h-screen w-full bg-card">
       <div className="absolute left-1/2 top-8 mx-auto -translate-x-1/2 transform justify-center">
         <Image src="/logo.png" alt="logo" width={100} height={100} />
       </div>
-      <div className="z-10 h-screen w-screen">{children}</div>
+      <div className="z-10 h-screen w-screen">
+        {user?.polarSubscriptionPending ? <Spinner /> : children}
+      </div>
       <div className="base-grid fixed h-screen w-screen opacity-40" />
       <div className="fixed bottom-0 h-screen w-screen bg-gradient-to-t from-[hsl(var(--card))] to-transparent" />
     </div>
