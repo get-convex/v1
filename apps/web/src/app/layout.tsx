052a5e4596b5b7a25df13328d9ccf1db6e5fdd9e
Shawn Erquhart <shawn@erquh.art>
2024-10-04 19:42:23 -0400

    update metadata url

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -15,7 +15,7 @@ const DepartureMono = localFont({
 });
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://v1.run"),
+  metadataBase: new URL("https://v1-convex.dev"),
   title: "Create v1",
   description:
     "A free, open-source starter kit for your next project, built with insights from Midday.",
