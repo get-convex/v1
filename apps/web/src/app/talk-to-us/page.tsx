2be717a0f063fe2bff07fffb958932ef26f8b6bd
Shawn Erquhart <shawn@erquh.art>
2024-10-08 09:56:59 -0400

    fix type error

--- apps/web/src/app/talk-to-us/page.tsx
+++ apps/web/src/app/talk-to-us/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
   return (
     <div className="flex flex-col items-center justify-center h-screen w-full">
       <div className="mt-24 w-full">
-        <CalEmbed calLink={env.NEXT_PUBLIC_CAL_LINK} />
+        <CalEmbed calLink={env.NEXT_PUBLIC_CAL_LINK || ""} />
       </div>
     </div>
   );
